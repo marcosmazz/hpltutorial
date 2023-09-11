92c0c5335f3f26d66be023aa14a8e326f89d1857
marcosmazz <marcos.mazzini@gmail.com>
2023-09-11 19:18:50 -0300

    initial commit

---
+++ build/build-env.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -euo pipefail
+
+SPACK_REPO="https://github.com/spack/spack.git"
+SPACK_TAG="releases/v0.20"
+
+export SPACK_USER_CONFIG_PATH=/var/empty
+
+echo Installing Spack
+cd ..
+git clone ${SPACK_REPO} spack || true
+cd spack
+git remote set-url origin ${SPACK_REPO}
+git fetch --all
+git reset --hard origin/${SPACK_TAG}
+cd ..
+source spack/share/spack/setup-env.sh
+
+echo Adding compilers to spack
+COMPILER_MODULES="gcc oneapi"
+module purge
+module load $COMPILER_MODULES
+spack compiler find --scope=site
+module purge
+
+echo Building apps
+cd build
+rm -rf .spack-env/ spack.lock
+spack env activate .
+spack concretize -f
+spack install --fresh -v -y
+spack module tcl refresh -y
+spack env deactivate
